4fbb15976bc389320a883ef3cf0bbc20607b6373
Pierre Reimertz <pierre@pierrereimertz.com>
2016-04-27 19:20:05 -0400

    stuff

--- js/scrollCheck.js
+++ js/scrollCheck.js
@@ -1,19 +1,20 @@
 var el = document.querySelector('.back-box'),
     elOffset = el.getBoundingClientRect().top,
-    isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent),
+    is
 
 function check(e) {
-  if(window.scrollY + 20 >= elOffset) {
-    el.style.position = 'fixed'
-    el.style.top = window.scrollY - 75 + 'px';
+  if(window.scrollY > 125) {
+    el.style.position = 'fixed';
+    el.style.top = '25px';
   }
   else {
-    el.style.position = 'absolute'
-    el.style.top = '50px';
+    el.style.position = 'absolute';
+    el.style.top = '150px';
   }
 }
 
 if(isMobile) {
   window.addEventListener('scroll', check.bind(this));
-  window.addEventListener('resize', check.bind(this));
+
 }
